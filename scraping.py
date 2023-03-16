dbb32e92c6701e7298da2ee66828500ce7c01fa4
Sean Fang <seanfang122@gmail.com>
2023-03-16 16:26:55 +0800

    spliting issue

--- scraping.py
+++ scraping.py
@@ -20,14 +20,15 @@ class scrape():
 
         assert '中國科技大學', 'moodle 學習平台' in self.driver.title
 
-    def enter_course(self):
+    def extracting(self):
         data = {
             'courseName': [],
             'assesmentName': [],
             'assesmentDetail': [],
-            'assesmentDate': []
+            'assesmentDate': [], 
+            'assesmentTime': {}
         }
-        for i in range(15):
+        for i in range(2, 3):
             title = self.driver.find_elements(By.TAG_NAME, 'h3')        #course title in the dashboard
             if title[i].text[:6] == '111(下)':
 
@@ -52,7 +53,14 @@ class scrape():
                             print('tag = ', k.get_attribute('innerHTML'))
                             if k.get_attribute('innerHTML') in [' 作業']:#, ' 測驗卷']:
                                 outerClass[j].click()
+                                self.driver.implicitly_wait(3)
 
+                                #get assesment detail
+                                # status = self.driver.find_element(By.XPATH, '//*[@id="region-main"]/div/div[2]/div[1]/table/tbody/tr[4]/td[2]')      #value of status in the assesment page
+                                # print(status.get_attribute('innerHTML'))
+                                # data['assesmentDate'].append(status)
+                                # print('已加入字典(作業狀態)###################')
+                                
                                 #get assesment detail
                                 detailList = []
                                 #status = self.driver.find_elements(By.CLASS_NAME, 'cell c1 lastcol')      #value of status in the assesment page
@@ -64,7 +72,8 @@ class scrape():
                                 data['assesmentDate'].append(detailList)
                                 print(detailList)
                                 print('已加入字典(作業狀態)###################')
-                                
+
+
                                 #get assesment name
                                 assesmentName = self.driver.find_element(By.TAG_NAME, 'h2')     #title in the assesment page
                                 print('此作業名子：', courseName+' | '+assesmentName.text)
@@ -72,7 +81,6 @@ class scrape():
                                 print('已加入字典(作業名子)###################')
 
                                 self.driver.back()
-                                #self.driver.implicitly_wait(3)
                                 break
                         break
                     print('forCount = ', forCount)
@@ -81,11 +89,37 @@ class scrape():
         time.sleep(10)
         self.driver.quit()
         return data 
+    
+
+    def split_date(self, data):
+        for i in range(len(data['assesmentDate'])):
+            timing = data['assesmentDate'][i].rsplit(' ', 1)
+            data['assesmentTime'][i] = timing[1]
+
+            date= data['assesmentDate'][i].split('(')
+            processed = date[0].split(' ')
+            data['assesmentDate'][i] = processed[0][:4] + processed[1][:2] + processed[2][:2]
+        print(data['assesmentDate'])
+        return data
+
+    #for 2D array
+    def split_date_2D(self, data):
+        for i in range(len(data['assesmentDate'][0])):
+            timing = data['assesmentDate'][0][i].rsplit(' ', 1)
+            data['assesmentTime'][i] = timing[1]
+
+            date= data['assesmentDate'][0][i].split('(')
+            processed = date[0].split(' ')
+            data['assesmentDate'][0][i] = processed[0][:4] + processed[1][:2] + processed[2][:2]
+        print(data['assesmentDate'])
+        return data
 
 
 def __main__():
     s = scrape()
-    s.enter_course()
+    data = s.extracting()
+    #s.split_date(data)
+    s.split_date_2D(data)
 
 if __name__ == '__main__':
     __main__()

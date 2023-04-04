5f6b2cec45fc31f4e87c68876595d41bf2c74e9e
Sean Fang <seanfang122@gmail.com>
2023-04-04 15:03:01 +0800

    refactor

---
+++ utils.py
@@ -0,0 +1,20 @@
+from datetime import datetime, date
+
+def checkDate(moodle_data, idx):
+    check_date_str = moodle_data['assessmentDueDate'][idx]
+    today = date.today()
+    check_date = datetime.strptime(check_date_str, '%Y-%m-%d').date()
+    if check_date >= today:return True
+    else:return False
+
+def setReminder(hWname):
+    if hWname.startswith('✅'):return False
+    else:return True
+
+def findIndex(hW_name, gHW_names):
+    for i in range(len(gHW_names)):
+        if hW_name == gHW_names[i]:
+            index = i
+            print('index = ', index, type(index))
+            break
+    return index

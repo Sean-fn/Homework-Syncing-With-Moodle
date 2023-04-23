5f8293b8260022d42c35c721e7b203b7e16f79b0
Sean Fang <seanfang122@gmail.com>
2023-04-23 12:21:32 +0800

    add css and js files

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 
 load_dotenv()
 
-app = Flask(__name__, static_url_path='/assets')
+app = Flask(__name__, template_folder='./frontend/templates',static_folder='./frontend/static')
 app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
 

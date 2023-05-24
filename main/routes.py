2d3733255713d318a4d7de1d35354eac6625d798
sean <sean@seans-MacBook-Air.local>
2023-05-24 17:01:17 +0800

    Feature: Adding delete user

--- main/routes.py
+++ main/routes.py
@@ -3,8 +3,8 @@ import json
 from flask import Blueprint, request, render_template
 
 from flask_api.common.utiles import Utiles
-from main.login_utiles import handle_successful_login, handle_login_failure
-from main.google_utiles import getGCredentials
+from main.login_utiles import handle_successful_login, handle_login_failure, handle_delete_account
+from main.dbUtiles import storeGCredentials
 
 
 main_bp = Blueprint('main', __name__)
@@ -19,7 +19,7 @@ class Routes:
             '''
             user_id = request.form['id']
             user_password = request.form['password']
-            gCredentials = getGCredentials(user_id, user_password)
+            gCredentials = storeGCredentials(user_id, user_password)
 
             try:
                 return handle_successful_login(gCredentials, user_id, user_password)
@@ -28,5 +28,13 @@ class Routes:
                 return handle_login_failure(e)
         
         return render_template('signup.html')
+    
+    #TODO: pops up error if the account not found
+    @main_bp.route('/delete-account', methods=['GET','POST'])
+    def delete_account():
+        if request.method == 'POST':
+            user_id = request.form['id']
+            return handle_delete_account(user_id)
+        return render_template('delete.html')
 
 Routes.main_bp = main_bp
